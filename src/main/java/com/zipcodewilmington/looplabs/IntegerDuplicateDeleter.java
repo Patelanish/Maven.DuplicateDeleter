19106d306bc131434c64de66f951c164858821d2
Anish Patel <patelanish223609@gmail.com>
2019-07-07 10:02:05 -0400

    Did it for loop way and Streams

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,26 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+
+    public IntegerDuplicateDeleter(Integer[] intArray){
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates (int MaxNumberOfDuplications) {
+        return Arrays.stream(array).filter(x -> Collections.frequency(Arrays.asList(array),x) < MaxNumberOfDuplications).toArray(Integer[]:: new);
+
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        return (Integer[]) Arrays.stream(array).filter(x -> Collections.frequency(Arrays.asList(array), x) != exactNumberOfDuplications).toArray();
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,8 +1,61 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+
+
+    public StringDuplicateDeleter(String[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        String[] value = new String[0];
+        String[] myArrays;
+        int numOfOccurance = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    numOfOccurance++;
+                }
+            }
+            if (numOfOccurance < maxNumberOfDuplications) {
+                myArrays = Arrays.copyOf(value, value.length + 1);
+                myArrays[myArrays.length - 1] = String.valueOf(array[i]);
+                value = myArrays;
+
+            }
+            numOfOccurance = 0;
+        }
+        return value;
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        String[] Values = new String[0];
+        String[] myArrays;
+        int numOfOccurance = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    numOfOccurance++;
+
+                }
+            }
+            if (numOfOccurance != exactNumberOfDuplications) {
+                myArrays = Arrays.copyOf(Values, Values.length + 1);
+                myArrays[myArrays.length - 1] = String.valueOf(array[i]);
+                Values = myArrays;
+
+            }
+            numOfOccurance = 0;
+        }
+        return Values;
+    }
 }
